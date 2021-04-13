11283cf036b32b7357d171ad3f286a9916538f12
Pieter <pieter@pfiers.net>
2021-04-14 00:33:20 +0200

    v1.1.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "net.pfiers.osmfocus"
         minSdkVersion(21)
         targetSdkVersion(30)
-        versionCode = 103
-        versionName = "1.0.3"
+        versionCode = 110
+        versionName = "1.1.0"
 
         multiDexEnabled = true
 
