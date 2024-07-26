8718a58cb29d4295b3b3dc23e1353409008430e1
Pieter Fiers <pieter@pfiers.net>
2024-07-26 20:06:25 +1200

    :bug: Handle HIDDEN note comments
    Also:
     - make note comment parser more lenient
     - allow for arbitrary note comment actions (to account for future or as of yet unseen ones)
    
    Fixes #53
    Fixes #54
    Fixes #56

--- app/src/main/java/net/pfiers/osmfocus/service/osmapi/conversion.kt
+++ app/src/main/java/net/pfiers/osmfocus/service/osmapi/conversion.kt
@@ -3,7 +3,22 @@ package net.pfiers.osmfocus.service.osmapi
 import com.beust.klaxon.JsonArray
 import com.beust.klaxon.JsonObject
 import com.beust.klaxon.Parser
-import net.pfiers.osmfocus.service.osm.*
+import net.pfiers.osmfocus.service.osm.Comment
+import net.pfiers.osmfocus.service.osm.Coordinate
+import net.pfiers.osmfocus.service.osm.Elements
+import net.pfiers.osmfocus.service.osm.ElementsMutable
+import net.pfiers.osmfocus.service.osm.Node
+import net.pfiers.osmfocus.service.osm.Note
+import net.pfiers.osmfocus.service.osm.NoteCommentAction
+import net.pfiers.osmfocus.service.osm.Notes
+import net.pfiers.osmfocus.service.osm.NotesMutable
+import net.pfiers.osmfocus.service.osm.Relation
+import net.pfiers.osmfocus.service.osm.RelationMember
+import net.pfiers.osmfocus.service.osm.TypedId
+import net.pfiers.osmfocus.service.osm.UsernameUidPair
+import net.pfiers.osmfocus.service.osm.Way
+import net.pfiers.osmfocus.service.osm.elementTypeFromString
+import net.pfiers.osmfocus.service.osm.setMerging
 import net.pfiers.osmfocus.service.util.boundedSubList
 import net.pfiers.osmfocus.service.util.iso8601DateTimeInUtcToInstant
 import net.pfiers.osmfocus.service.util.osmCommentDateTimeToInstant
@@ -30,6 +45,7 @@ fun jsonToElements(
             val elementObj = elementAny as JsonObject
             val type = elementObj["type"] as String
             val id = (elementObj["id"] as Number).toLong()
+
             val version = elementObj["version"] as Int
             val tags = elementObj["tags"]?.let { t ->
                 (t as JsonObject).toMap().mapValues { (_, v) -> v as String }
@@ -48,27 +64,31 @@ fun jsonToElements(
                     mergedUniverse.setMerging(id, node)
                     newElements[id] = node
                 }
+
                 "way" -> {
-                    val wayNodes = (elementObj["nodes"] as JsonArray<*>).map { e ->
+                    val wayNodes = (elementObj["nodes"] as JsonArray<*>?)?.map { e ->
                         (e as Number).toLong()
-                    }
+                    } ?: emptyList()
                     val way = Way(version, tags, wayNodes, changeset, timestamp, username)
                     mergedUniverse.setMerging(id, way)
                     newElements[id] = way
                 }
+
                 "relation" -> {
-                    val members = (elementObj["members"] as JsonArray<*>).map { e ->
+                    val members = (elementObj["members"] as JsonArray<*>?)?.map { e ->
                         val memberObj = e as JsonObject
                         val ref = (memberObj["ref"] as Number).toLong()
                         val memberType = elementTypeFromString(memberObj["type"] as String)
                         val role = memberObj["role"] as String
                         RelationMember(TypedId(ref, memberType), role)
-                    }
-                    val relation = Relation(version, tags, members, changeset, timestamp, username)
+                    } ?: emptyList()
+                    val relation =
+                        Relation(version, tags, members, changeset, timestamp, username)
                     mergedUniverse.setMerging(id, relation)
                     newElements[id] = relation
                 }
-                else -> throw OsmApiParseException("Unrecognised element type: $type")
+
+                else -> Timber.w("Unrecognised element type: $type (id=$id)")
             }
         }
     } catch (ccEx: ClassCastException) {
@@ -88,12 +108,12 @@ open class OpeningCommentData(
 )
 
 class CommentData(
-    val actionStr: String,
+    val action: NoteCommentAction,
     timestamp: Instant,
     usernameUidPair: UsernameUidPair?,
     text: String,
     html: String
-): OpeningCommentData(timestamp, usernameUidPair, text, html)
+) : OpeningCommentData(timestamp, usernameUidPair, text, html)
 
 fun jsonToNotes(
     json: String,
@@ -108,58 +128,63 @@ fun jsonToNotes(
         val noteFeatures = rootObj["features"] as JsonArray<*>
         for (noteFeatureAny in noteFeatures) {
             val noteFeature = noteFeatureAny as JsonObject
-
-            val noteGeometry = noteFeature["geometry"] as JsonObject
-            val coordinateArray = noteGeometry["coordinates"] as JsonArray<*>
-            val coordinate = Coordinate(coordinateArray[1] as Double, coordinateArray[0] as Double)
-
             val noteProperties = noteFeature["properties"] as JsonObject
-
             val id = (noteProperties["id"] as Number).toLong()
 
-            val commentDataList = (noteProperties["comments"] as JsonArray<*>).map { commentAny ->
-                jsonObjectToCommentData(commentAny as JsonObject)
-            }
-            val sorted = commentDataList.sortedBy { comment -> comment.timestamp }
-            if (sorted != commentDataList) {
-                throw OsmApiParseException("Comments are not sorted by date")
-            }
+            try {
+                val noteGeometry = noteFeature["geometry"] as JsonObject
+                val coordinateArray = noteGeometry["coordinates"] as JsonArray<*>
+                val coordinate =
+                    Coordinate(coordinateArray[1] as Double, coordinateArray[0] as Double)
 
-            val openingCommentData = commentDataList.firstOrNull()?.let { openingCommentData ->
-                when (openingCommentData.actionStr) {
-                    "OPENED" -> Unit
-                    // The first comment action of some older notes is "COMMENTED"
-                    // instead of "OPENED" (e.g. https://api.openstreetmap.org/api/0.6/notes/757593.json)
-                    NoteCommentAction.COMMENTED.name -> Timber.w(
-                        "Old-style note: opening comment uses action \"COMMENTED\" instead of \"OPENED\" (id=$id)"
-                    )
-                    else -> throw OsmApiParseException(
-                        "First note comment action is not OPENED or COMMENTED (is ${openingCommentData.actionStr} instead, id=$id)"
-                    )
+                val commentDataList =
+                    (noteProperties["comments"] as JsonArray<*>).map { commentAny ->
+                        jsonObjectToCommentData(commentAny as JsonObject)
+                    }
+                val sorted = commentDataList.sortedBy { comment -> comment.timestamp }
+                if (sorted != commentDataList) {
+                    Timber.d("Note comments are not sorted by date (id=$id)")
                 }
-                openingCommentData
-            }?: run {
-                Timber.w("Invalid note: empty comments list, assuming empty creation description (id=$id)")
-                val fallbackCreationDate = osmCommentDateTimeToInstant(noteProperties["date_created"] as String)
-                // TODO: Get username/uid from note itself?
-                OpeningCommentData(fallbackCreationDate, null, "", "")
-            }
+                val openingCommentData = sorted.firstOrNull()?.let { openingCommentData ->
+                    when (openingCommentData.action) {
+                        NoteCommentAction.Unknown("OPENED") -> Unit
+                        // e.g. https://api.openstreetmap.org/api/0.6/notes/757593.json
+                        NoteCommentAction.Known.COMMENTED -> Timber.d(
+                            "Old-style note: opening comment uses action \"COMMENTED\" instead of \"OPENED\" (id=$id)"
+                        )
+                        // e.g. https://api.openstreetmap.org/api/0.6/notes/1866523.json
+                        NoteCommentAction.Known.CLOSED -> Timber.d(
+                            "Note with purged history: opening comment is \"CLOSED\" (id=$id)"
+                        )
 
-            val comments = commentDataList.boundedSubList(1).map { d ->
-                val action = NoteCommentAction.valueOf(d.actionStr)
-                Comment(d.timestamp, d.usernameUidPair, action, d.text, d.html)
-            }
+                        else -> Timber.d(
+                            "Unexpected first note comment action (${openingCommentData.action.value} instead, id=$id)"
+                        )
+                    }
+                    openingCommentData
+                } ?: run {
+                    Timber.d("Invalid note: empty comments list, assuming empty creation description (id=$id)")
+                    val fallbackCreationDate =
+                        osmCommentDateTimeToInstant(noteProperties["date_created"] as String)
+                    OpeningCommentData(fallbackCreationDate, null, "", "")
+                }
+                val comments = commentDataList.boundedSubList(1).map { d ->
+                    Comment(d.timestamp, d.usernameUidPair, d.action, d.text, d.html)
+                }
 
-            val note = Note(
-                coordinate,
-                comments,
-                openingCommentData.usernameUidPair,
-                openingCommentData.timestamp,
-                openingCommentData.text,
-                openingCommentData.html
-            )
-            mergedUniverse.setMerging(id, note)
-            newElements[id] = note
+                val note = Note(
+                    coordinate,
+                    comments,
+                    openingCommentData.usernameUidPair,
+                    openingCommentData.timestamp,
+                    openingCommentData.text,
+                    openingCommentData.html
+                )
+                mergedUniverse.setMerging(id, note)
+                newElements[id] = note
+            } catch (exception: Exception) {
+                Timber.w("Exception while parsing note (id=$id)")
+            }
         }
     } catch (ccEx: ClassCastException) {
         throw OsmApiParseException("Undefined property or property with wrong type", ccEx)
@@ -173,8 +198,8 @@ fun jsonObjectToCommentData(commentObject: JsonObject): CommentData {
     val usernameUidPair = (commentObject["uid"] as Number?)?.toLong()?.let { uid ->
         UsernameUidPair(uid, commentObject["user"] as String)
     }
-    val actionStr = (commentObject["action"] as String).uppercase()
+    val action = NoteCommentAction.valueOf(commentObject["action"] as String)
     val text = commentObject["text"] as String
     val html = commentObject["html"] as String
-    return CommentData(actionStr, timestamp, usernameUidPair, text, html)
+    return CommentData(action, timestamp, usernameUidPair, text, html)
 }
