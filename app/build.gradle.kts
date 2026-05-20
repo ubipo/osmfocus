61f0f929bf42c57ba38348088e4163e0b821d1a0
Pieter Fiers <pieter@pfiers.net>
2026-05-20 11:18:49 +0200

    chore: modernize AGP config

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -128,13 +128,13 @@ android {
         create("fdroid") {
             dimension = distributionChannelDimension
             versionNameSuffix = "-fdroid"
-            signingConfigs.asMap["release"]?.let { signingConfig = it }
+            signingConfigs["release"]?.let { signingConfig = it }
         }
 
         create("gplay") {
             dimension = distributionChannelDimension
             versionNameSuffix = "-gplay"
-            signingConfigs.asMap["release"]?.let { signingConfig = it }
+            signingConfigs["release"]?.let { signingConfig = it }
         }
     }
 
